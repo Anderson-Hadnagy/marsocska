6f06215823e48f5ba3e7b4d293afdf3ca74b0689
koptersanyi <koptersanyi@DESKTOP-M83DS45>
2026-03-02 21:49:27 +0100

    .

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
 function createWindow() {
     const win = new BrowserWindow({
+        devTools: true,
         width: 1200,
         height: 800,
         frame: true,
         titleBarStyle: 'hidden',
         transparent: true,
-        resizable: true,
+        resizable: false,
+        fullscreenable: false,
         backgroundColor: 'rgba(0, 0, 0, 0)',
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
@@ -18,7 +20,6 @@ function createWindow() {
   win.loadFile('index.html');
 }
 
-const { ipcMain } = require('electron');
 
 ipcMain.on('minimize', (event) => {
   BrowserWindow.getFocusedWindow().minimize();
