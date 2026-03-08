892f053eb0b1e9ee5bd44c6b1f7621959e794932
Anderson-Hadnagy <zsiros.andras.gabor@gmail.com>
2026-03-08 20:55:46 +0100

    Update main.tsx
    Removed unnecessary comments about window controls.

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -4,9 +4,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
-// 2. We securely attach the window controls here to bypass Electron's security blocks
 document.getElementById('btn-minimize')?.addEventListener('click', () => {
-  // We use (window as any) so TypeScript doesn't panic about custom APIs
   (window as any).api.minimize()
 })
 
